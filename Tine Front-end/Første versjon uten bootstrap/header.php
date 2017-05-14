b8c4c0466c2d35eb8fad0620cda2c99a0e15c390
tinesol <soltin16@student.westerdals.no>
2017-05-14 23:55:48 +0200

    Ny versjon laget i bootstrap
    -ordenet ny mappe med gammel og ny versjon
    -laget forside på nytt i bootstrap
    -noe forbedringer kan gjøres

--- Tine Front-end/Første versjon uten bootstrap/header.php
+++ Tine Front-end/Første versjon uten bootstrap/header.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" type="text/css" href="Stylesheets/hovedside.css"/>
+<link rel="stylesheet" type="text/css" href="stylesheets/hovedside.css"/>
     
 <title>Westerdals</title>
 
