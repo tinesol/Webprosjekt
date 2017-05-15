3d4c0afe85d7a55475da3e8d949a7a12e187a0fd
tinesol <soltin16@student.westerdals.no>
2017-05-15 15:57:36 +0200

    Ryddet i mappe og samlet koden med det phylis har gjort

---
+++ Front-end/Første versjon uten bootstrap/footer.php
@@ -0,0 +1,9 @@
+    <footer>
+      <p>Contact us.</p>
+      <p>Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.</p>
+      <br><br>
+      <p>(c) Copyright 2017</p>
+    </footer>
+
+</body>
+</html>
---
+++ Front-end/Første versjon uten bootstrap/hovedside.php
@@ -0,0 +1,107 @@
+<?php require 'header.php' ?>
+    
+    <div class="searchbox">
+        <input type="text" name="search" class="search" placeholder="Search...">
+    </div>
+    
+    <div class="categoriesbox">
+    <div class="icon">
+        <a href="#home">
+        <img src="Images/COLOURBOX95430431.jpg">
+        <span>Utesteder</span>
+        </a>
+    </div>
+    <div class="icon">
+        <a href="#home">
+        <img src="Images/COLOURBOX95430431.jpg">
+        <span>Biblioteker</span>
+        </a>
+    </div>
+    <div class="icon">
+        <a href="#home">
+        <img src="Images/COLOURBOX95430431.jpg">
+        <span>Parker</span>
+        </a>
+    </div>
+    <div class="icon">
+        <a href="#home">
+        <img src="Images/COLOURBOX95430431.jpg">
+        <span>Trening</span>
+        </a>
+    </div>
+    <div class="icon">
+        <a href="#home">
+        <img src="Images/COLOURBOX95430431.jpg">
+        <span>Konserter</span>
+        </a>
+    </div>
+    </div>
+    
+    <div class="eventbox">
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+        <div class="event">
+        <a href="#event">
+        <img src="Images/COLOURBOX15287877.jpg">
+        <h5>Event</h5>
+        <h6>12:00-24:00</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+        </a>
+        </div>
+    </div>
+    
+<?php require 'footer.php' ?>
