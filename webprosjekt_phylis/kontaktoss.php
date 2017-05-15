f74d86d0c33a8b8f4799515896688c2cffc8aec4
phylisbg <boaphy16@student.westerdals.no>
2017-05-15 15:05:47 +0200

    Add files via upload

---
+++ webprosjekt_phylis/kontaktoss.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    
+    <title>Kontakt Oss</title>
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        body {padding-top:100px;}
+    </style>
+    <!-- <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script> -->
+    <!-- <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script> -->
+</head>
+
+<body>
+<div class="container">
+	<div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="well well-sm">
+          <form class="form-horizontal" action="" method="post">
+          <fieldset>
+            <legend class="text-center">Kontakt Oss</legend>
+    
+            <!-- Name input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="navn">Navn</label>
+              <div class="col-md-9">
+                <input id="navn" navn="navn" type="text" placeholder="Ditt navn" class="form-control">
+              </div>
+            </div>
+    
+            <!-- Email input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="">E-post</label>
+              <div class="col-md-9">
+                <input id="e-post" name="e-post" type="text" placeholder="Din e-post" class="form-control">
+              </div>
+            </div>
+    
+            <!-- Message body -->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="message">Melding</label>
+              <div class="col-md-9">
+                <textarea class="form-control" id="melding" name="melding" placeholder="Vennligst skriv melding her..." rows="5"></textarea>
+              </div>
+            </div>
+    
+            <!-- Form actions -->
+            <div class="form-group">
+              <div class="col-md-12 text-right">
+                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+              </div>
+            </div>
+          </fieldset>
+          </form>
+        </div>
+      </div>
+	</div>
+</div>
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
