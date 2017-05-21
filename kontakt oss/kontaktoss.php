01a6a4a88651b1709c39e861e7c97dad73fb4428
phylisbg <boaphy16@student.westerdals.no>
2017-05-21 17:46:20 +0200

    Add files via upload

---
+++ kontakt oss/kontaktoss.php
@@ -0,0 +1,50 @@
+<?php require "../header/header.php"?>
+
+<div class="container">
+<div class="row">
+    <div style="margin-top: 50px;">
+    <div class="col-md-6 col-md-offset-3">
+    <div class="well well-sm">
+        <form class="form-horizontal" action="" method="post">
+        <fieldset>
+        <legend class="text-center">CONTACT US</legend>
+    
+            <!-- Name input-->
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="navn">Name</label>
+                <div class="col-md-9">
+                    <input id="navn" navn="navn" type="text" placeholder="Your name" class="form-control">
+                </div>
+            </div>
+    
+            <!-- Email input-->
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="">E-post</label>
+                <div class="col-md-9">
+                    <input id="e-post" name="Email" type="text" placeholder="Your email" class="form-control">
+                </div>
+            </div>
+    
+            <!-- Message body -->
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="message">Message</label>
+                <div class="col-md-9">
+                    <textarea class="form-control" id="melding" name="melding" placeholder="Your message..." rows="5"></textarea>
+                </div>
+            </div>
+    
+            <!-- Form actions -->
+            <div class="form-group">
+                <div class="col-md-12 text-right">
+                    <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                </div>
+            </div>
+        </fieldset>
+        </form>
+    </div>
+    </div>
+    </div>
+</div>
+</div>
+
+<?php require "../footer/footer.php"?>
